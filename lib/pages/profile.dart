d73b3afc32efe5a5930974451817e678df2a1402
M3 Beatz <m3beatz@M3.fios-router.home>
2024-02-22 21:09:29 -0500

    Stable, working on dark/light theme before continuing to reserve functions, then host functions, then live functions

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,12 +2,9 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:keyboard_actions/keyboard_actions.dart';
 import 'package:slotted/common/colors.dart';
 import 'package:slotted/common/slotted_user.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ProfilePage extends StatefulWidget {
