91e4ed67c16c6ef299fca2303257050ae2327341
M3 Beatz <m3beatz@M3.fios-router.home>
2024-02-27 09:08:13 -0500

    Added notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:slotted/api/firebase_options.dart';
 import 'package:slotted/common/colors.dart';
 import 'package:slotted/pages/main_nav.dart';
@@ -28,10 +32,70 @@ void main() async {
   runApp(const MyApp(debug: _debug));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key, this.debug = false});
 
   final bool debug;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> showNotification(String title, String body) async {
+    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
+    var platformChannelSpecifics =
+        NotificationDetails(iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      DateTime.now().millisecondsSinceEpoch, // Notification ID
+      title, // Notification title
+      body, // Notification body
+      platformChannelSpecifics,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    final DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+      onDidReceiveLocalNotification:
+          (int id, String? title, String? body, String? payload) async {
+        // Handle notification when app is in foreground on iOS
+      },
+    );
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      iOS: initializationSettingsIOS,
+    );
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("onMessage: $message");
+      RemoteNotification? notification = message.notification;
+      AppleNotification? iOS = message.notification?.apple;
+
+      if (notification != null && iOS != null) {
+        // Display the notification
+        showNotification(notification.title!, notification.body!);
+      }
+      // Handle the message when the app is in the foreground
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print("onMessageOpenedApp: $message");
+      // Handle the message when the app is opened from a terminated state
+    });
+  }
 
   // This widget is the root of your application.
   @override
@@ -49,8 +113,32 @@ class MyApp extends StatelessWidget {
       ),
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) =>
-            MainNav(user: snapshot.data, debug: debug),
+        builder: (context, snapshot) {
+          if (snapshot.data?.uid != null) {
+            Permission.notification.request().then((status) {
+              if (status.isGranted) {
+                // Permission is granted
+                print('Notification permissions granted');
+                _firebaseMessaging.getAPNSToken().then((apnsToken) {
+                  return _firebaseMessaging.getToken();
+                }).then((token) {
+                  FirebaseFirestore.instance
+                      .doc('users/${snapshot.data!.uid}')
+                      .set(
+                    {
+                      'pushToken': token,
+                    },
+                    SetOptions(merge: true),
+                  );
+                });
+              } else {
+                // Permission is denied
+                print('Notification permissions denied');
+              }
+            });
+          }
+          return MainNav(user: snapshot.data, debug: widget.debug);
+        },
       ),
     );
   }
